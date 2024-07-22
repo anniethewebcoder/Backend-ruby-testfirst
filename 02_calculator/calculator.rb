c5c9b63d4c3096d6d658183a13e671c2df1d7a9a
anniethewebcoder <anniethewebcoder@gmail.com>
2024-07-21 20:54:28 -0400

    temp, calc, simon

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,43 @@
 #write your code here
+
+def add(x,y)
+    return x + y
+end
+
+def subtract(x,y)
+    return x - y
+end
+
+def sum(a)
+    # result = a.reduce(0) {|t, e| t + e}
+    # return result
+    return a.sum
+end
+
+def multiply(a)
+    result = a.reduce(1) {|t, e| t * e}
+    return result
+end
+
+def power(x,y)
+    # result = 1
+
+    # for i in 1..y
+    #     result *= x
+    # end
+
+    # return result
+
+    return x ** y
+end
+
+def factorial(x)
+    result = 1
+    
+    for i in 1..x
+        result *= (i)
+    end
+
+    return result
+
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(x)
+    return x
+end
+
+def shout(x)
+    return x.upcase
+end
+
+def repeat(x, y=2)
+
+    result = (x + " ") * y
+    return result.chop
+end
+
+def start_of_word(x, y)
+    return x.slice(0, y)
+end
+
+def first_word(x)
+    return x.split(" ")[0]
+end
+
+def titleize(x)
+    little_words = [
+        "a", "to", "by", "in", "and", "but", "or", "the", "on", "with", "at", "an"
+    ]
+
+    cap_x = x.split(" ").map{ |w| w.capitalize }.join(" ")
+    return cap_x
+
+end
+
