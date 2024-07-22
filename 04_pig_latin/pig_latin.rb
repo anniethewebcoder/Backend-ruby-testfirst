911dfcd1479dd651b362cc4fd62c5d5786ec2a83
anniethewebcoder <annie.the.web.coder@gmail.com>
2024-07-22 16:59:25 -0400

    pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,20 @@
 #write your code here
+def translate(x)
+
+    vowels = ["a", "e", "i", "o", "u"]
+
+    result = ""
+
+    if vowels.include?(x[0]) == true 
+        result = x + "ay"
+    else
+        first_letter = x[0]
+        rest_of_word = x[1..-1]
+        
+        result = rest_of_word + first_letter + "ay"
+    end
+
+    return result
+
+
+end
