ae33c061d69cabacae3fce8c114f950c456578d0
anniethewebcoder <anniethewebcoder@gmail.com>
2024-07-21 23:09:01 -0400

    simon titleize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -23,14 +23,15 @@ end
 
 def titleize(x)
     little_words = [
-        "a", "to", "by", "in", "and", "but", "or", "the", "on", "with", "at", "an"
+        "a", "to", "by", "in", "and", "but", "or", "the", "on", "with", "at", "an", "over"
     ]
 
     cap_x = x.split(" ")
     
     result = ""
-    cap_x.each do |w|
-       if little_words.include?(w) == true
+
+    cap_x.each_with_index do |w, i|
+       if little_words.include?(w) == true && i > 0 
         result += w + " "
        else 
         result += w.capitalize + " "
