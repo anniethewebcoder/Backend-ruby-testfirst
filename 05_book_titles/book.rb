500cbfbb99c0b285c9bb4143c1c8931ca295bdae
anniethewebcoder <anniethewebcoder@gmail.com>
2024-07-23 17:38:12 -0400

    titleize

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,30 @@
 class Book
 # write your code here
+    attr_accessor :title
+
+    def title=(new_title)
+        @title = titleize(new_title)
+    end
+
+    def titleize(x)
+        little_words = [
+            "a", "to", "by", "in", "and", "but", "or", "the", "on", "with", "at", "an", "over", "of"
+        ]
+    
+        cap_x = x.split(" ")
+        
+        result = ""
+    
+        cap_x.each_with_index do |w, i|
+           if little_words.include?(w) == true && i > 0 
+            result += w + " "
+           else 
+            result += w.capitalize + " "
+           end
+        end
+        
+        return result.chop
+    
+    end
+    
 end
